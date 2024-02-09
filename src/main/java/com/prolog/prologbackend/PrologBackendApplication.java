1ddfd8214051d39f3d80b6eea6f8188af514f0e7
Choi-Young-Eun <cye8891@naver.com>
2024-02-09 20:21:35 +0900

    Create Project

---
+++ src/main/java/com/prolog/prologbackend/PrologBackendApplication.java
@@ -0,0 +1,13 @@
+package com.prolog.prologbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrologBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrologBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prolog/prologbackend/PrologBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prolog.prologbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrologBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
