f911977aad4e052163221354284cc78501563b38
Choi-Young-Eun <cye8891@naver.com>
2024-02-21 23:54:22 +0900

    Feat: 로그인 시 인증 처리 추가
    -커스텀 인증 필터 추가
    -jwt 타입 및 생성과정 추가
    -커스텀 UserDetails 파일 추가
    
    관련 issue: #6, #8, #9

--- src/main/java/com/prolog/prologbackend/Config/SpringSecurityConfig.java
+++ src/main/java/com/prolog/prologbackend/Config/SpringSecurityConfig.java
@@ -1,7 +1,11 @@
 package com.prolog.prologbackend.Config;
 
+import com.prolog.prologbackend.Security.Authentication.CustomAuthenticationFilter;
+import com.prolog.prologbackend.Security.Jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,21 +16,35 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SpringSecurityConfig {
+    private final JwtProvider jwtProvider;
+    private final AuthenticationConfiguration authenticationConfiguration;
+
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.formLogin(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .logout(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((authorize) -> authorize.requestMatchers("/**").permitAll())
                 .sessionManagement(sessionManagement ->
-                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((authorize) -> authorize.requestMatchers("/**").permitAll())
+                .addFilter(customAuthenticationFilter());
+
         return http.build();
     }
 
     @Bean
     PasswordEncoder setPasswordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CustomAuthenticationFilter customAuthenticationFilter() throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter =
+                new CustomAuthenticationFilter(authenticationConfiguration.getAuthenticationManager(),jwtProvider);
+        customAuthenticationFilter.setFilterProcessesUrl("/users/login");
+        return customAuthenticationFilter;
     }
 }
---
+++ src/main/java/com/prolog/prologbackend/Security/Jwt/JwtType.java
@@ -0,0 +1,18 @@
+package com.prolog.prologbackend.Security.Jwt;
+
+import lombok.Getter;
+
+public enum JwtType {
+    ACCESS_TOKEN("Access Token",600000),
+    REFRESH_TOKEN("Refresh Token",1800000);
+
+    @Getter
+    private String tokenType;
+    @Getter
+    private int expirationTime;
+
+    JwtType(String tokenType, int expirationTime){
+        this.tokenType = tokenType;
+        this.expirationTime = expirationTime;
+    }
+}
