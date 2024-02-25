f9149df33637c1069e29d510eb42e4aa6bc21332
Choi-Young-Eun <cye8891@naver.com>
2024-02-26 00:28:31 +0900

    Feat: 회원 간단 정보 조회 기능 추가
    -Controller 내 메서드 추가
    -SimpleResponseDto 추가
    
    resolve : #4

---
+++ src/main/java/com/prolog/prologbackend/Member/DTO/Response/SimpleMemberDto.java
@@ -0,0 +1,23 @@
+package com.prolog.prologbackend.Member.DTO.Response;
+
+import com.prolog.prologbackend.Member.Domain.Member;
+import lombok.Getter;
+
+@Getter
+public class SimpleMemberDto {
+    private String nickName;
+    private String profileImage;
+    private String email;
+    private String phone;
+
+    public static SimpleMemberDto of(Member member){
+        SimpleMemberDto memberDto = new SimpleMemberDto();
+
+        memberDto.nickName = member.getNickname();
+        memberDto.profileImage = member.getProfileImage();
+        memberDto.email = member.getEmail();
+        memberDto.phone = member.getPhone();
+
+        return memberDto;
+    }
+}
