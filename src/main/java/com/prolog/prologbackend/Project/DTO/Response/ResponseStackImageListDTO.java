cf5ac1ef17016051cbcfe19e5a4470deb1c64fc4
Kim Gwang Je <102839139+KimGwangJe@users.noreply.github.com>
2024-03-13 16:48:41 +0900

    Update ResponseStackImageListDTO.java

--- src/main/java/com/prolog/prologbackend/Project/DTO/Response/ResponseStackImageListDTO.java
+++ src/main/java/com/prolog/prologbackend/Project/DTO/Response/ResponseStackImageListDTO.java
@@ -1,7 +1,30 @@
 package com.prolog.prologbackend.Project.DTO.Response;
 
-public class ResponseStackImageDTO {
-    private Long stackId;
-    private String stackName;
-    private String stackUrl;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class ResponseStackImageListDTO {
+
+    @Schema(description = "스택 이미지 리스트", nullable = false, example = "리스트로 된 ResponseStackImageDTO")
+    private List<ResponseStackImageDTO> stackImageList;
+
+    @Getter
+    @Setter
+    public static class ResponseStackImageDTO{
+
+        @Schema(description = "스택 이미지의 ID", nullable = false, example = "1")
+        private Long stackId;
+
+        @Schema(description = "스택 이미지의 이름", nullable = false, example = "React or 리액트")
+        private String stackName;
+
+        @Schema(description = "스택 이미지의 링크", nullable = false, example = "www.~~~")
+        private String image;
+    }
+
 }
