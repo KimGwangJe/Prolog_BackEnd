392d61bd86acc22f41d8e7b8b08c7a1aa88693bd
Choi-Young-Eun <cye8891@naver.com>
2024-03-21 20:47:08 +0900

    Docs: JWT 문서화 설정 추가
    related to : #40

--- src/main/java/com/prolog/prologbackend/Config/SwaggerConfig.java
+++ src/main/java/com/prolog/prologbackend/Config/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package com.prolog.prologbackend.Config;
 
+import com.prolog.prologbackend.Security.Jwt.JwtType;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,8 +14,34 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
+        String accessKey = JwtType.ACCESS_TOKEN.getTokenType();
+        String refreshKey = JwtType.REFRESH_TOKEN.getTokenType();
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList(accessKey)
+                .addList(refreshKey);
+
+        SecurityScheme accessTokenSecurityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .scheme("Bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name(accessKey);
+
+        SecurityScheme refreshTokenSecurityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .scheme("Bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name(refreshKey);
+
+        Components components = new Components()
+                .addSecuritySchemes(accessKey, accessTokenSecurityScheme)
+                .addSecuritySchemes(refreshKey, refreshTokenSecurityScheme);
+
         return new OpenAPI()
-                .components(new Components())
+                .addSecurityItem(securityRequirement)
+                .components(components)
                 .info(apiInfo());
     }
 
